803d756a23dd6cfb3eb477ce8fe787ea23bf85fd
Sachindra Kr Thakur <47210942+sachindra01@users.noreply.github.com>
2021-05-03 00:26:56 +0545

    column widget added

---
+++ lib/View/setting/credit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Credit extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('sachidnra')
+        ],
+      ),
+    );
+  }
+}
